36fd65278eb1309e1cc448841cbe93aaf6ab17ff
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-02-21 01:03:12 +0530

    added state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,63 @@
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import Inventory from "./components/Inventory";
-import { Routes, Route } from "react-router-dom";
-import SalesExecutives from "./components/SalesExecutives";
-import CreateOrder from "./components/CreateOrder";
-import Orders from "./components/Orders";
 import LoginPage from "./components/LoginPage";
 import AdminPage from "./pages/AdminPage";
 import SalesMan from "./pages/SalesMan";
-import { useEffect, useState } from "react";
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Get value from localStorage or use default
+      state: localStorage.getItem("state") || "login",
+    };
+    // Listen to storage event
+    window.addEventListener("storage", (e) => this.storageChanged(e));
+    // Bind this to storageChanged()
+    this.storageChanged = this.storageChanged.bind(this);
+  }
 
-function App() {
-  const [admin, SetAdmin] = useState(true);
-   const [state, setState] = useState("login");
-  useEffect(()=>{
-    setState(localStorage.state?localStorage.state:'login');
-  },[]);
-  return (
-    <>
-      {state === "login" ? (
-        <LoginPage state={state} setState={setState} />
-      ) : null}
-      {state === "admin" ? (
-        <AdminPage state={state} setState={setState} />
-      ) : null}
-      {state === "sales" ? (
-        <SalesMan state={state} setState={setState} />
-      ) : null}
-    </>
-  );
+  storageChanged(e) {
+    console.log(e.key);
+    if (e.key === "state") {
+      this.setState({ state: e.newValue });
+    }
+  }
+  render() {
+    return (
+      <>
+        {this.state.state === "login" ? (
+          <LoginPage state={this.state.state} />
+        ) : null}
+        {this.state.state === "admin" ? (
+          <AdminPage state={this.state.state} />
+        ) : null}
+        {this.state.state === "sales" ? (
+          <SalesMan state={this.state.state} />
+        ) : null}
+      </>
+    );
+  }
 }
 
 export default App;
+
+// function App() {
+//    const [state, setState] = useState("login");
+//   useEffect(()=>{
+//     setState(localStorage.state?localStorage.state:'login');
+//   },[]);
+//   return (
+//     <>
+//       {state === "login" ? (
+//         <LoginPage state={state} setState={setState} />
+//       ) : null}
+//       {state === "admin" ? (
+//         <AdminPage state={state} setState={setState} />
+//       ) : null}
+//       {state === "sales" ? (
+//         <SalesMan state={state} setState={setState} />
+//       ) : null}
+//     </>
+//   );
+// }
+
+// export default App;
