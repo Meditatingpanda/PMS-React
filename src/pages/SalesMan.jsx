e2bc9b924da6fa36a8942a45656b38fd4be47f8d
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-02-21 14:55:21 +0530

    changes done

--- src/pages/SalesMan.jsx
+++ src/pages/SalesMan.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
-import Inventory from "../components/Inventory";
 import { Routes, Route, Navigate } from "react-router-dom";
-import SalesExecutives from "../components/SalesExecutives";
 import CreateOrder from "../components/CreateOrder";
 import Orders from "../components/Orders";
 import SidebarSales from "../components/SidebarSales";
@@ -16,6 +13,7 @@ function SalesMan({ state, logoutHandler }) {
         <SidebarSales />
         <Routes>
           <Route exact path="/" element={<Navigate to="/create" />} />
+          <Route exact path="/sales" element={<Navigate to="/create" />} />
           <Route exact path="/create" element={<CreateOrder />} />
           <Route exact path="/orders" element={<Orders />} />
         </Routes>
